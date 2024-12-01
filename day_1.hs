94914705c21812b6576172b4ca56562c71c3840a
Lyo Nya Zhee-Zhin <lazhizhin@yandex.ru>
2024-12-01 19:55:11 +0100

    Day 1

---
+++ day_1.hs
@@ -0,0 +1,48 @@
+import Data.List (sort)
+
+main = do
+  contents <- readFile "day_1.txt"
+  let records = parseRecords contents
+  print $ partOne records
+  print $ partTwo records
+
+parseInts :: [String] -> [Integer]
+parseInts = map read
+
+parseRecords :: String -> [[Integer]]
+parseRecords content = map (parseInts . words) (lines content)
+
+absDiff :: Integer -> Integer -> Integer
+absDiff a b = max a b - min a b
+
+partOne :: [[Integer]] -> Integer
+partOne records =
+  let diffs = zipWith absDiff first second
+      first = sort $ map (!! 0) records
+      second = sort $ map (!! 1) records
+   in sum diffs
+
+countOccurances :: [Integer] -> [(Integer, Integer)]
+countOccurances [] = []
+countOccurances [x] = [(x, 1)]
+countOccurances (x : xs) =
+  let head_count = fromIntegral $ (length . filter (== x)) xs
+      head = (x, head_count + 1)
+      tail = countOccurances $ filter (/= x) xs
+   in head : tail
+
+matchOccurances :: [(Integer, Integer)] -> [Integer] -> Integer
+matchOccurances [] _ = 0
+matchOccurances [(x, cnt)] v =
+  let other_cnt = fromIntegral $ length $ filter (== x) v
+   in x * cnt * other_cnt
+matchOccurances ((x, cnt) : xs) v =
+  let other_cnt = fromIntegral $ length $ filter (== x) v
+      tail = matchOccurances xs (filter (/= x) v)
+   in x * cnt * other_cnt + tail
+
+partTwo :: [[Integer]] -> Integer
+partTwo records =
+  let first = map (!! 0) records
+      second = map (!! 1) records
+   in matchOccurances (countOccurances first) second
