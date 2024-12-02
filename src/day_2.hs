66db3e98176d2b640b4f70da139e42c7e08afaf0
Lyo Nya Zhee-Zhin <lazhizhin@yandex.ru>
2024-12-02 16:06:47 +0100

    Day 2

---
+++ src/day_2.hs
@@ -0,0 +1,34 @@
+import Data.List (sort)
+import System.Environment qualified as SE
+
+main = do
+  args <- SE.getArgs
+  contents <- readFile $ head args
+  let records = parseRecords contents
+  print $ partOne records
+  print $ partTwo records
+
+partOne :: [[Integer]] -> Int
+partOne records = length $ filter isSafe records
+
+partTwo :: [[Integer]] -> Int
+partTwo records = length $ filter (\v -> isSafe v || canBeSafe v) records
+
+parseInts :: [String] -> [Integer]
+parseInts = map read
+
+parseRecords :: String -> [[Integer]]
+parseRecords content = map (parseInts . words) (lines content)
+
+isSafe :: [Integer] -> Bool
+isSafe x =
+  let diffs = zipWith subtract x (tail x)
+   in (all (< 0) diffs && all (> -4) diffs) || (all (> 0) diffs && all (< 4) diffs)
+
+canBeSafe :: [Integer] -> Bool
+canBeSafe x = any (isSafe . removeElement x) [0 .. length x]
+
+removeElement :: [a] -> Int -> [a]
+removeElement [] _ = []
+removeElement (x : xs) 0 = xs
+removeElement (x : xs) n = x : removeElement xs (n - 1)
